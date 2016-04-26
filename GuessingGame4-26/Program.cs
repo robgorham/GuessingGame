d33f385a53205586add4e1cd44838d9be5a9bb4c
Robert Gorham <robsteeliano@gmail.com>
2016-04-26 10:09:21 -0400

    Initial Commit

---
+++ GuessingGame4-26/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessingGame4_26
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+        }
+
+
+
+        static void run()
+        {
+            bool finished = false;
+            while(!finished)
+            {
+                finished = false;
+
+            }
+        }
+    }
+}
